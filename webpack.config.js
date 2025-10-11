915f609e08c77277732cfda0af61f3ac5508b4b3
Kevin Hyde <kphyde@gmail.com>
2025-10-11 16:10:29 -0700

    add new character images in WebP format; update image paths in MainInterface.js and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        test: /\.(png|jpg|jpeg|gif|svg|webp)$/,
         type: 'asset/resource',
         generator: {
           filename: 'images/[name][ext]'
